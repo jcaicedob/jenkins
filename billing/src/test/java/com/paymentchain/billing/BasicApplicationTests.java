a61517e9dee9b1175bd34c0fb22d7b22ca7fe8ef
Jairo Caicedo <jcaice@hotmail.com>
2022-02-15 06:51:48 -0800

    a ver si por fin

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-            String message= "default message cambio test devops";
+            String message= "default message cambio test";
             Assert.assertNotNull(message);
 	}
 
