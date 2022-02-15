1dd631ca2aa37b9d5f81279457a9e962b7b8c0b2
Jairo Caicedo <jcaice@hotmail.com>
2022-02-15 06:31:15 -0800

    add test modification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,5 +1,6 @@
 package com.paymentchain.billing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +12,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-            String message= "this is a default message";
+            String message= "default message cambio test devops";
             Assert.assertNotNull(message);
 	}
 
